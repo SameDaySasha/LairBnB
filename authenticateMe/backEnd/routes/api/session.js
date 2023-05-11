fbaa547100db0b9bb8f9fc9234f076c76ea0aecc
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-05-11 17:30:50 -0400

    Phase5p1

--- authenticateMe/backEnd/routes/api/session.js
+++ authenticateMe/backEnd/routes/api/session.js
@@ -5,15 +5,31 @@ const bcrypt = require('bcryptjs');
 const router = express.Router();
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
-
-
+// backend/routes/api/session.js
+// ...
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 // backend/routes/api/session.js
 // ...
 
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
+
+
 // Log in
 router.post(
   '/',
+  validateLogin,
   async (req, res, next) => {
     const { credential, password } = req.body;
 
@@ -47,9 +63,6 @@ router.post(
     });
   }
 );
-
-
-
 
 
 module.exports = router;
