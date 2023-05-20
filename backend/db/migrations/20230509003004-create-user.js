9fd91b1620b76fa9e044fa9ed5de182e9e7fc6b9
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-05-20 18:55:12 -0400

    CherynFix

--- backend/db/migrations/20230509003004-create-user.js
+++ backend/db/migrations/20230509003004-create-user.js
@@ -9,7 +9,8 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
+    options.tableName = 'Users'
+    return queryInterface.createTable(options, {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -48,9 +49,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
-    });
+    },options);
   },
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
+    options.tableName = 'Users'
+    return queryInterface.dropTable(options);
   },
 };
