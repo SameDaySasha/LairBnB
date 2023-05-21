6b6b36777e0caa83144bdb53234a048f54bca1c3
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-05-20 20:10:00 -0400

    MMaybe???

--- backend/db/migrations/20230518173855-create-spot.js
+++ backend/db/migrations/20230518173855-create-spot.js
@@ -3,14 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 
 let options = {};
-options.tableName = 'Spots'
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable(options, {
+    await queryInterface.createTable('Spots', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -79,6 +78,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable(options);
+    await queryInterface.dropTable('Spots');
   },
 };
