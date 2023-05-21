22f0d1122de404c71c55e8d7668cf45e5a7df2f3
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-05-20 20:03:46 -0400

    MMaybe???

--- backend/db/migrations/20230518175543-create-booking.js
+++ backend/db/migrations/20230518175543-create-booking.js
@@ -52,7 +52,6 @@ module.exports = {
     },options);
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = 'Bookings'
-    await queryInterface.dropTable(options);
+    await queryInterface.dropTable('Bookings');
   }
 };
