77d5850a5f645b919985fcd23c76ee7c444b1459
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-05-22 18:05:23 -0400

    MMaybe???

--- backend/db/migrations/20230519193226-create-review.js
+++ backend/db/migrations/20230519193226-create-review.js
@@ -1,13 +1,14 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 let options = {};
+options.tableName
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Reviews', {
+    await queryInterface.createTable(options.tableName, {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -46,9 +47,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    },options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Reviews');
+    await queryInterface.dropTable(options.tableName);
   }
 };
