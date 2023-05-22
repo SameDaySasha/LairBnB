c8921270027dc5491df2d8966c41ccf9f510be8b
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-05-22 13:35:41 -0400

    MMaybe???

--- backend/db/migrations/20230519193225-create-image.js
+++ backend/db/migrations/20230519193225-create-image.js
@@ -2,13 +2,14 @@
 
 /** @type {import('sequelize-cli').Migration} */
 let options = {};
+options.tableName = 'Images'
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Images', {
+    await queryInterface.createTable(option.tableName, {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -43,7 +44,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE,
       },
-    });
+    },options);
 
     // if (process.env.NODE_ENV === 'production') {
     //   await queryInterface.addIndex('Images', ['indexId', 'indexType']);
@@ -51,6 +52,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Images');
+    await queryInterface.dropTable(options.tableName);
   },
 };
