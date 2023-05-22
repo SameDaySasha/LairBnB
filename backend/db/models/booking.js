ce9b3a2c7cdff2f824f9a70af87e3979c6b650e1
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-05-22 14:56:35 -0400

    MMaybe???

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -1,118 +1,220 @@
 'use strict';
-const { Model, DataTypes } = require('sequelize');
-module.exports = (sequelize) => {
-  class Booking extends Model {
-    static associate(models) {
+const { Model, DataTypes, Op } = require('sequelize');
 
-      Booking.belongsTo(models.Spot, {
-        foreignKey: 'spotId',
-        otherKey:'id'
-      });
-      Booking.belongsTo(models.User, {
-        foreignKey: 'userId',
-        otherKey:'id'
-      });
-      // Booking.hasOne(models.Spot,
-      //   {foreignKey:'id',
-      //     otherKey:'spotId'})
-
-      // Booking.hasOne(models.User,{
-      //   foreignKey:'id',
-      //   otherKey:'UserId'
-      // })
-      // Booking.belongsTo(models.Spot, {
-      //   foreignKey: 'id',
-      //   onDelete: 'CASCADE',
-      // });
-      // Booking.belongsTo(models.User, {
-      //   foreignKey: 'id',
-      //   onDelete: 'CASCADE',
-      // });
-      // Add any additional associations as needed
-    }
+class Booking extends Model {
+  static associate(models) {
+    Booking.belongsTo(models.Spot, {
+      foreignKey: 'spotId',
+      onDelete: 'CASCADE',
+    });
+    Booking.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE',
+    });
   }
 
-  Booking.init(
-    {
-      spotId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-          notNull: {
-            msg: 'Spot ID is required.',
+  static init(sequelize) {
+    return super.init(
+      {
+        spotId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          validate: {
+            notNull: {
+              msg: 'Spot ID is required.',
+            },
+            isInt: {
+              msg: 'Spot ID must be an integer.',
+            },
           },
-          isInt: {
-            msg: 'Spot ID must be an integer.',
+        },
+        userId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          validate: {
+            notNull: {
+              msg: 'User ID is required.',
+            },
+            isInt: {
+              msg: 'User ID must be an integer.',
+            },
+          },
+        },
+        startDate: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          validate: {
+            notNull: {
+              msg: 'Start date is required.',
+            },
+            isDate: {
+              msg: 'Start date must be a valid date.',
+            },
+          },
+        },
+        endDate: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          validate: {
+            notNull: {
+              msg: 'End date is required.',
+            },
+            isDate: {
+              msg: 'End date must be a valid date.',
+            },
+          },
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          validate: {
+            notNull: {
+              msg: 'Created date is required.',
+            },
+            isDate: {
+              msg: 'Created date must be a valid date.',
+            },
+          },
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          validate: {
+            notNull: {
+              msg: 'Updated date is required.',
+            },
+            isDate: {
+              msg: 'Updated date must be a valid date.',
+            },
           },
         },
       },
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-          notNull: {
-            msg: 'User ID is required.',
-          },
-          isInt: {
-            msg: 'User ID must be an integer.',
-          },
-        },
-      },
-      startDate: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        validate: {
-          notNull: {
-            msg: 'Start date is required.',
-          },
-          isDate: {
-            msg: 'Start date must be a valid date.',
-          },
-        },
-      },
-      endDate: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        validate: {
-          notNull: {
-            msg: 'End date is required.',
-          },
-          isDate: {
-            msg: 'End date must be a valid date.',
-          },
-        },
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        validate: {
-          notNull: {
-            msg: 'Created date is required.',
-          },
-          isDate: {
-            msg: 'Created date must be a valid date.',
-          },
-        },
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        validate: {
-          notNull: {
-            msg: 'Updated date is required.',
-          },
-          isDate: {
-            msg: 'Updated date must be a valid date.',
-          },
-        },
-      },
-    },
-    {
-      sequelize,
-      modelName: 'Booking',
-      tableName: 'bookings',
-    }
-  );
+      {
+        sequelize,
+        modelName: 'Booking',
+        tableName: 'bookings',
+      }
+    );
+  }
 
-  return Booking;
-};
+  static associate(models) {
+    Booking.belongsTo(models.Spot, {
+      foreignKey: 'spotId',
+      onDelete: 'CASCADE',
+    });
+    Booking.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE',
+    });
+  }
+
+  static init(sequelize) {
+    return super.init(
+      {
+        spotId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          validate: {
+            notNull: {
+              msg: 'Spot ID is required.',
+            },
+            isInt: {
+              msg: 'Spot ID must be an integer.',
+            },
+          },
+        },
+        userId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          validate: {
+            notNull: {
+              msg: 'User ID is required.',
+            },
+            isInt: {
+              msg: 'User ID must be an integer.',
+            },
+          },
+        },
+        startDate: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          validate: {
+            notNull: {
+              msg: 'Start date is required.',
+            },
+            isDate: {
+              msg: 'Start date must be a valid date.',
+            },
+          },
+        },
+        endDate: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          validate: {
+            notNull: {
+              msg: 'End date is required.',
+            },
+            isDate: {
+              msg: 'End date must be a valid date.',
+            },
+          },
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          validate: {
+            notNull: {
+              msg: 'Created date is required.',
+            },
+            isDate: {
+              msg: 'Created date must be a valid date.',
+            },
+          },
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          validate: {
+            notNull: {
+              msg: 'Updated date is required.',
+            },
+            isDate: {
+              msg: 'Updated date must be a valid date.',
+            },
+          },
+        },
+      },
+      {
+        sequelize,
+        modelName: 'Booking',
+        tableName: 'bookings',
+      }
+    );
+  }
+
+  static async validateBookingConflict(startDate, endDate, spotId) {
+    const existingBooking = await Booking.findOne({
+      where: {
+        spotId,
+        [Op.or]: [
+          {
+            startDate: {
+              [Op.between]: [startDate, endDate],
+            },
+          },
+          {
+            endDate: {
+              [Op.between]: [startDate, endDate],
+            },
+          },
+        ],
+      },
+    });
+
+    if (existingBooking) {
+      throw new Error('Booking conflict detected.');
+    }
+  }
+}
+
+module.exports = Booking;
