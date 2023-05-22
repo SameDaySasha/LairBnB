41c8d75872ab6e4cefda515244655cbb0b35da71
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-05-22 13:05:00 -0400

    MMaybe???

--- backend/db/migrations/20230519175543-create-booking.js
+++ backend/db/migrations/20230519175543-create-booking.js
@@ -5,7 +5,7 @@ options.tableName = 'Bookings'
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA; // define your schema in options object
 }
-
+console.log("I DONT KNOW WHAT THE FUCK IM DOING ")
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable(options, {
