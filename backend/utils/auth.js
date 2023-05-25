85eb55d537298c700937679b45a683da1b2b161c
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-05-25 18:40:43 -0400

    DoesitWorkNow

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -72,17 +72,15 @@ const restoreUser = (req, res, next) => {
 // ...
 
 // If there is no current user, return an error
-// If there is no current user, return an error
-const requireAuth = (req, _res, next) => {
-  if (req.user) return next();
-
-  const err = new Error('Authentication required');
-  err.title = 'Authentication required';
-  err.errors = { message: 'Authentication required' };
-  err.status = 401;
-  next(err); // Call next with the error
-};
-
+const requireAuth = function (req, _res, next) {
+    if (req.user) return next();
+  
+    const err = new Error('Authentication required');
+    err.title = 'Authentication required';
+    err.errors = { message: 'Authentication required' };
+    err.status = 401;
+    return next(err);
+  }
 
   // backend/utils/auth.js
 // ...
