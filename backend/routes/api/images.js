c7406e691eb90d9649309519808e351093b36be4
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-05-27 18:11:51 -0400

    WhoYouGonnaCall???

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -3,7 +3,8 @@ const { requireAuth } = require('../../utils/auth');
 const { Spot, Image } = require('../../db/models');
 const router = express.Router();
 
-// DELETE /images/:imageId - Delete an existing image for a Spot
+
+// DELETE /images/:imageId - Delete an existing image for a Spot or Review
 router.delete('/:imageId', requireAuth, async (req, res) => {
   const imageId = parseInt(req.params.imageId, 10);
 
@@ -14,17 +15,29 @@ router.delete('/:imageId', requireAuth, async (req, res) => {
     // Check if the image exists
     if (!image) {
       return res.status(404).json({
-        message: "Spot Image couldn't be found"
+        message: "Image couldn't be found"
       });
     }
 
-    // Fetch the corresponding spot
-    const spot = await Spot.findByPk(image.spotId);
-
-    // Check if the spot exists and belongs to the current user
-    if (!spot || spot.ownerId !== req.user.id) {
-      return res.status(403).json({
-        message: "You don't have permission to delete this Spot Image"
+    // Fetch the corresponding spot or review based on the image's indexType
+    let parent;
+    if (image.indexType === 'Spot') {
+      parent = await Spot.findByPk(image.indexId);
+      if (!parent || parent.ownerId !== req.user.id) {
+        return res.status(403).json({
+          message: "You don't have permission to delete this Spot Image"
+        });
+      }
+    } else if (image.indexType === 'Review') {
+      parent = await Review.findByPk(image.indexId);
+      if (!parent || parent.userId !== req.user.id) {
+        return res.status(403).json({
+          message: "You don't have permission to delete this Review Image"
+        });
+      }
+    } else {
+      return res.status(400).json({
+        message: "Invalid indexType"
       });
     }
 
@@ -45,46 +58,98 @@ router.delete('/:imageId', requireAuth, async (req, res) => {
 });
 
 
-// DELETE /images/:imageId - Delete an existing image for a Review
-router.delete('/:imageId', requireAuth, async (req, res) => {
-    const imageId = parseInt(req.params.imageId, 10);
+
+
+
+
+
+
+
+
+
+// I'm apprehensive to delete anything and therefore am a code hoarder 
+// // DELETE /images/:imageId - Delete an existing image for a Spot
+// router.delete('/:imageId', requireAuth, async (req, res) => {
+//   const imageId = parseInt(req.params.indexId, 10);
+
+//   try {
+//     // Find the image to be deleted
+//     const image = await Image.findByPk(imageId);
+
+//     // Check if the image exists
+//     if (!image) {
+//       return res.status(404).json({
+//         message: "Spot Image couldn't be found"
+//       });
+//     }
+
+//     // Fetch the corresponding spot
+//     const spot = await Spot.findByPk(image.spotId);
+
+//     // Check if the spot exists and belongs to the current user
+//     if (!spot || spot.ownerId !== req.user.id) {
+//       return res.status(403).json({
+//         message: "You don't have permission to delete this Spot Image"
+//       });
+//     }
+
+//     // Delete the image
+//     await image.destroy();
+
+//     // Send the successful response
+//     return res.status(200).json({
+//       message: "Successfully deleted"
+//     });
+//   } catch (error) {
+//     // Handle any errors that occur during the request
+//     console.error(error);
+//     return res.status(500).json({
+//       message: 'Internal server error'
+//     });
+//   }
+// });
+
+
+// // DELETE /images/:imageId - Delete an existing image for a Review
+// router.delete('/:imageId', requireAuth, async (req, res) => {
+//     const imageId = parseInt(req.params.imageId, 10);
   
-    try {
-      // Find the image to be deleted
-      const image = await Image.findByPk(imageId);
+//     try {
+//       // Find the image to be deleted
+//       const image = await Image.findByPk(imageId);
   
-      // Check if the image exists
-      if (!image) {
-        return res.status(404).json({
-          message: "Review Image couldn't be found"
-        });
-      }
+//       // Check if the image exists
+//       if (!image) {
+//         return res.status(404).json({
+//           message: "Review Image couldn't be found"
+//         });
+//       }
   
-      // Fetch the corresponding review
-      const review = await Review.findByPk(image.reviewId);
+//       // Fetch the corresponding review
+//       const review = await Review.findByPk(image.reviewId);
   
-      // Check if the review exists and belongs to the current user
-      if (!review || review.userId !== req.user.id) {
-        return res.status(403).json({
-          message: "You don't have permission to delete this Review Image"
-        });
-      }
+//       // Check if the review exists and belongs to the current user
+//       if (!review || review.userId !== req.user.id) {
+//         return res.status(403).json({
+//           message: "You don't have permission to delete this Review Image"
+//         });
+//       }
   
-      // Delete the image
-      await image.destroy();
+//       // Delete the image
+//       await image.destroy();
   
-      // Send the successful response
-      return res.status(200).json({
-        message: "Successfully deleted"
-      });
-    } catch (error) {
-      // Handle any errors that occur during the request
-      console.error(error);
-      return res.status(500).json({
-        message: 'Internal server error'
-      });
-    }
-  });
+//       // Send the successful response
+//       return res.status(200).json({
+//         message: "Successfully deleted"
+//       });
+//     } catch (error) {
+//       // Handle any errors that occur during the request
+//       console.error(error);
+//       return res.status(500).json({
+//         message: 'Internal server error'
+//       });
+//     }
+//   });
   
 
 
