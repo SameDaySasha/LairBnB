6129786c5a4ec5ae7e239f7f68b866079c68640b
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-07-15 17:06:24 -0400

    VersionOnePointSix

--- frontend/src/components/ConfirmDeleteModal/index.js
+++ frontend/src/components/ConfirmDeleteModal/index.js
@@ -3,12 +3,17 @@ import { useModal } from "../../context/Modal";
 function ConfirmDeleteModal({ onConfirm, itemType }) {
   const { closeModal } = useModal();
 
+  const handleClose = () => {
+    closeModal();
+    window.location.reload();
+  };
+
   return (
     <div>
       <h2>Confirm Delete</h2>
       <p>Are you sure you want to delete this {itemType}?</p>
       <button onClick={onConfirm}>Yes (Delete {itemType})</button>
-      <button onClick={closeModal}>No (Keep {itemType})</button>
+      <button onClick={handleClose}>No (Keep {itemType})</button>
     </div>
   );
 }
